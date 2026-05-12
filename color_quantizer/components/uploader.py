e63f43187a59efa0a489c073682e877486af2a32
Iván David Romero More <121291844+Idromerom714@users.noreply.github.com>
2026-05-12 20:13:28 +0000

    Only reset last_result when uploading a new file

--- color_quantizer/components/uploader.py
+++ color_quantizer/components/uploader.py
@@ -10,7 +10,10 @@ def render_uploader() -> Optional["numpy.ndarray"]:
     if uploaded is not None:
         arr = load_image(uploaded)
         st.session_state["original_array"] = arr
+        prev_name = st.session_state.get("filename")
+        # Only clear last_result when a different file is uploaded
+        if prev_name is None or prev_name != uploaded.name:
+            st.session_state["last_result"] = None
         st.session_state["filename"] = uploaded.name
-        st.session_state["last_result"] = None
         return arr
     return None
